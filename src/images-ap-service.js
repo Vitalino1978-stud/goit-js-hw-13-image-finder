22f70b75b1a067cd58595d2905eecef7b410a1e2
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-03-18 01:10:18 +0200

    works, but must add scrollTo

--- src/images-ap-service.js
+++ src/images-ap-service.js
@@ -1,39 +1,26 @@
-// import { get, set } from "core-js/core/dict";
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '20659518-d870fa33769225566579aec48';
-export default class ImageSearch {
+export default class GetImageAPI {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
   }
-
-
   fetchImages() {
-    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
-    return fetch(url)
-      
+    const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=3&key=20575585-f86bba565132ad4f87d3b8fdb`;
+    return fetch(URL)
       .then(response => response.json())
-      .then(({ hits }) => {
-       this.incrementPage()
-        return hits;
+      .then(data => {
+        // console.log(data);
+        this.page += 1;
+        return data.hits;
       });
-       
   }
-  
-  incrementPage() {
-    this.page += 1;
-  }
-  resetPage() {
-    this.page = 1;
-  }
-
   get query() {
     return this.searchQuery;
   }
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
+  resetPage() {
+    this.page = 1;
+  }
 }
-
-///
--- src/index.js
+++ src/index.js
@@ -1,50 +1,48 @@
 
-import imageCardTemplate from "../tempates/image-cards.hbs";
-import ImageSearch from "./images-ap-service";
- import LoadMoreBtn from "./load-next-page-btn";
-
-
+// import '../../css/styles.css';
+import LoadMoreBtn from './load-next-page-btn';
+import API from './images-ap-service';
+import galleryTemplate from '../tempates/image-cards.hbs';
 const refs = {
-  searhForm: document.querySelector('.search-form'),
-  gallery: document.querySelector('.gallery'),
-}
-
-refs.searhForm.addEventListener('submit', onSearch)
- 
-
-const imageSearch = new ImageSearch();
-const loadMoreBtn = new LoadMoreBtn({selector: '[data-action="load-more"]', hidden: true,
+  searchForm: document.querySelector('.search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+};
+const api = new API();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
 });
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
+refs.searchForm.addEventListener('submit', onSearch);
 
-function onSearch(event) {
-  event.preventDefault();
-  
-  imageSearch.query = event.currentTarget.elements.query.value;
-  if (imageSearch.query === '') {
-    return alert('Please enter correct data.');
-  }
-
-  // loadMoreBtn.show();
-  imageSearch.resetPage();
-  clearGallery();
-  fetchImages();
-}
-  
-
-function fetchImages() {
-  // loadMoreBtn.disable();
-    imageSearch.fetchImages().then(images => {
-    appendImageCardsMarkup(images);
-    // loadMoreBtn.enable();
+function onSearch(e) {
+  e.preventDefault();
+  api.query = e.currentTarget.elements.query.value;
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
+  api.resetPage();
+ 
+  api.fetchImages().then(images => {
+    cleargalleryContainer();
+    appendArticlesMarkup(images);
+    loadMoreBtn.enable();
   });
-};
+  
+}
 
-
-function appendImageCardsMarkup(hits) {
-  refs.gallery.insertAdjacentHTML("beforeend", imageCardTemplate(hits))
-};
-
-function clearGallery() {
-  refs.gallery.innerHTML = ""
-};
-///
+function onLoadMore() {
+  loadMoreBtn.disable();
+  api.fetchImages().then(images => {
+    appendArticlesMarkup(images);
+    loadMoreBtn.enable();
+  });
+}
+function appendArticlesMarkup(images) {
+  refs.galleryContainer.insertAdjacentHTML(
+    'beforeend',
+    galleryTemplate(images),
+  );
+}
+function cleargalleryContainer() {
+  refs.galleryContainer.innerHTML = '';
+}
